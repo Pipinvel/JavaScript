92a1e14545512b2a78ae4556180662e9b56b521a
Pipinvel <samayajoroeva06@gmail.com>
2022-12-07 19:45:14 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,517 @@
+"use strict";
+//alert("Hello,world!");
+
+//1
+// let num;
+// num=123;
+// alert (num);
+
+/* 2 
+let a, b, c;
+a=1;
+b=6;
+c=3;*/
+
+// 3
+// let num;
+// num= 10;
+// alert (num);
+// num = 20;
+// alert (num);
+
+// 1 (мат.операции)
+// let a = 1+2+3;
+// alert (a);
+
+// 8.1
+// let a = 10;
+// let b = 2;
+// alert (a+b);
+// alert(a-b);
+// alert (a*b);
+// alert(a/b);
+
+// 8.2
+// let c=10;
+// let d=5;
+// let result = c+d;
+// alert(result);
+
+// 8.3
+// let a=1;
+// let b=2;
+// let c=3;
+// alert(a+b+c);
+
+// 8.4
+// let a=10;
+// let b=5;
+// let c=a-b;
+// let d=7;
+// let result=c+d;
+// alert(result);
+
+//9.1
+// let a=5+5*3;
+// alert(a);
+
+//9.2
+// let a=5+5*3+3;
+// alert(a);
+
+// let a = 8 + 2 / 2;
+// alert(a);
+
+// let a = 8 * 4 / 2 / 2;
+// alert(a);
+
+// let a = (2 + 3) * (2 + 3);
+// alert(a);
+// let a = (2 + 3) * 2 + 3;
+// alert(a);
+
+// let a = 2 * (2 + 4 * (3 + 1));
+// alert(a);
+
+// 10.1
+// let a=1.5;
+// let b=0.75;
+// alert(a+b);
+
+// 11.1
+// let a=-100;
+// alert(a);
+
+// 11.2
+// let a=-1;
+// alert(-a);
+
+//12.1
+// let a=13;
+// let b=5;
+// alert(a%b);
+
+// 13.1
+// let a=2;
+// alert(a ** 10);
+
+// let a = 3 * 2 ** 3;
+// alert(a);
+
+// let a = (3 * 2) ** 3;
+// alert(a);
+
+// let a = 3 * 2 ** (3 + 1);
+// alert(a);
+// let a = 3 * 2 ** 3 * 3;
+// alert(a);
+
+// 14.1
+// let name='Samaya';
+// let surname='Joroeva';
+// alert(name + surname);
+
+// 15.1
+// let str='!!!';
+// alert(str);
+
+// let str='hello';
+// let str2='world';
+// alert(str+ ' ' + str2);
+
+// 16.1
+// let str='i hate people';
+// alert(str.length);
+
+//17.1
+// let str1 = 'xxx';                                                     // !!!!!!!!
+// let str2 = 'yyy';
+// let txt  = `aaa ${str1}`; //bbb ${str2} ccc`;
+
+// 17.2
+// let str=`a
+// b
+// c`;
+// alert(str);
+
+// 18.1
+// let str;
+// alert(str);
+
+// 18.2
+// let a=null;
+// alert(a);
+
+// let a=true;
+// let b=false;
+// alert(a);
+// alert(b);
+
+// 18.5
+// let str1="djfhsib";
+// let str2="kjhdk";
+// alert(str1 * str2);
+
+// 18.6,7
+// let a=10;
+// alert(a/0);
+// alert(-a/0);
+
+// 19
+// let a=1234;
+// console.log(a);
+
+// let a=1;
+// let b=2;
+// a=3;
+// console.log(a, b);
+
+// console.log(123);
+// console.log('123');
+// console.log(true);
+// console.log(Infinity);
+// console.log(false);
+
+// console.log(a);
+
+//20.1
+// const PI=3.14;
+// let r=4;
+// console.log(2*r*PI);
+
+//21.1
+// let a = '5' + '2';
+// alert(a);
+
+// let a = '5' + 2;
+// alert(a);
+// let a = 5 + 2;
+// alert(a);
+// let a = '5' * '2';
+// alert(a);
+// let a = '5' - '2';
+// alert(a);
+// let a = '5' / '2';
+// alert(a);
+// let a = '5' % '2';
+// alert(a);
+// let a = '5s' * '2';
+// alert(a);
+//let a = '5s' + '2';
+//alert(a);
+// let a = (-'5') + (-'2');
+// alert(a);
+// let a = '5' * 1 + '2' * 1;
+// alert(a);
+// let a = '5' * '1' + '2' * '1';
+// alert(a);
+// let a = '' + 3 + 1;
+// alert(a);
+
+//22.1
+// let a='10';
+// let b='20';
+// alert(Number(a) + Number(b));
+
+//22.2
+// alert( Number('2') + Number('3') );
+//22.3
+//alert( 2 + Number('3') );
+//22.4
+// alert( '2' + Number('3') );
+
+//22.5
+// let a = +'2';
+// let b = +'3';
+// alert(a + b); 
+
+//23.1
+// let a=parseInt('5px');
+// let b=parseInt('6px');
+//alert(a+b);
+//23.2
+// let str=parseFloat('5.5px');
+// let str2=parseFloat('6.25px');
+// alert(str+str2+"px");
+
+//24.1
+// let str=String(12);
+// let str2=String(34);
+// alert(str+str2);
+//24.2
+//let a=123456789;
+//alert(String(a).length);
+//24.3
+// let a = 2146;
+// let b = 437569;
+// alert(String(a).length+String(b).length);
+
+//25.1
+// alert(true + 3);
+// alert(true - true);
+// alert(true + false);
+// alert('1' + true);
+// alert( String(true) + 1 );
+// alert( String(true) + Number(true) );
+
+//26
+// let test = Boolean(3);
+// alert(test);
+//let test = Boolean(0);
+//alert(test);
+//let test = Boolean(true);
+//alert(test);
+// let test = Boolean(+0);
+// alert(test);
+// let test = Boolean(-1);
+// alert(test);
+// let test = Boolean(-0);
+// alert(test);
+// let test = Boolean('true');
+// alert(test);
+// let test = Boolean('false');
+// alert(test);
+// let test = Boolean(null);
+// alert(test);
+// let test = Boolean(undefined);
+// alert(test);
+// let test = Boolean(3 * 'abc');
+// alert(test);
+//let test = Boolean(1 / 0);
+//alert(test);
+
+//27.1
+// let str='abcde';
+// alert(str[0]);
+// alert(str[2]);
+// alert(str[4]);
+//27.2
+// let str='abcde';
+// let result= str[4]+str[3]+str[2]+str[1]+str[0];
+// alert(result);
+//27.3
+// let str='abcde';
+// let num=2;
+// alert(str[num]);
+
+//28.1
+//let str='dzhfh;elbvzsletuy';
+//alert(str[str.length-1]);
+//28.2
+// let str='abcdesngiziguwh';
+// alert(str[str.length-2]);
+//28.3
+// let str='abcdesngiziguwh';
+// alert(str[str.length-3]);
+
+//29.1
+// let str = '12345';
+// alert(Number(str[0])+Number(str[1])+Number(str[2])+Number(str[3])+Number(str[4]));
+//29.2
+// let a = String(12345);
+// alert(Number(a[0])+Number(a[1])+Number(a[2])+Number(a[3])+Number(a[4]));
+//29.3
+// let a = String(12345);
+// alert(a[0]*a[1]*a[2]*a[3]*a[4]);
+//29.4
+// let a = String(12345);
+// alert(a[4]+a[3]+a[2]+a[1]+a[0]);
+
+//30.1
+// let num = 1;
+// num = num + 1;
+// num = num + 1;
+
+// alert(num);
+//30.2
+// let num = 1;
+// num = num + 2;
+// num = num + 3;
+
+// alert(num);
+//30.3
+// let num = 47;
+// num += 7;
+// num -=18;
+// num *=10;
+// num /=15;
+// alert(num);
+
+//31.1
+// let num = 10;
+// num++;
+// num++;
+// num--;
+// alert(num);
+//31.2
+// let num = 3;
+// alert(++num);
+//31.3
+// let num = 3;
+// alert(num++);
+//31.4
+// let num = 3;
+// alert(--num);
+//31.5
+// let num = 3;
+// alert(num--);
+//31.6
+// let num1 = 3;
+// let num2 = ++num1;
+// alert(num1);
+// alert(num2);
+//31.7
+// let num1 = 3;
+// let num2 = num1++;
+// alert(num1);
+// alert(num2);
+//31.8
+// let num1 = 3;
+// let num2 = --num1;
+// alert(num1);
+// alert(num2);
+//31.9
+// let num1 = 3;
+// let num2 = num1--;
+// alert(num1);
+// alert(num2);
+//31.10
+// let num1 = 3;
+// num1++;
+// let num2 = num1--;
+// alert(num1++);
+// alert(--num2);
+//
+
+//32.1
+// alert(0.1 * 0.2);
+// alert(toFixed(0.3) - toFixed(0.1));
+
+//33.1
+// let a = prompt('Ваш возраст?');
+// alert('Ваш возраст:' + ' ' +a);
+//33.2
+// let num1 = prompt('Введите первое число');
+// let num2 = prompt('Введите второе число');
+// alert(+num1 + +num2); 
+//33.3
+// let side=prompt('Введите сторону квадрата');
+// alert('Площадь квадрата равна:'+ (side*side));
+//33.4
+// let a=prompt('Введите 1 сторону прямоугольника');
+// let b=prompt('Введите 2 сторону прямоугольника');
+// alert('Периметр прямоугольника равен:' + ' ' + (2*(+a + +b)));
+
+//34.1
+//document.write('some text');
+//34.2
+//document.write('<i>some text</i>');
+//34.3
+//let str = 'text';
+//document.write('<i>' + str + '</i>');
+//34.4
+// document.write(1 + '<br>');
+// document.write(2 + '<br>');
+// document.write(3 + '<br>');
+// document.write(4 + '<br>');
+// document.write(5);
+
+//35.1
+// let num1 = 1;
+// let num2 = 2;
+// console.log('сумма: ' + (num1 + num2));
+//35.2
+// let a = 1;
+// let b = 2;
+// console.log(a + b);
+//35.3
+// let num = '123';
+// let sum = +num[0] + +num[1] + +num[2];
+// console.log(sum);
+//35.4
+// let num = 123;
+// let str = String(num)
+// console.log(str[0]);
+//35.5
+// let a = 0;
+// console.log(++a);
+//35.6
+// let num = 123;
+// console.log(String(num).length);
+//35.7
+// let a = 24 * 60 * 60;
+// console.log(a);
+//35.8
+// let num = 123;
+// let str = String(num);
+// console.log(str.length);
+//35.9
+// let num = 123;
+// let str = String(num);
+// console.log(str[str.length-1]);
+//35.10
+// let num = 123;
+// let str = String(num);
+// console.log(str.length);
+//35.11
+// let num = 123;
+// let str = String(num);
+// console.log(str[str.length-1]);
+//35.12
+// let a = '123';
+// let b = '456';
+// let s = Number(a) + Number(b);
+// console.log(s);
+//35.13
+// let aaa = 1;
+// let bbb = 2;
+// let ccc = 3;
+// console.log(aaa + bbb + ccc);
+
+//36.1
+//alert(24*60*60);
+//36.2
+//alert(30*24*60*60);
+//36.3
+//alert(365*24*60*60);
+//36.4
+//alert(24*60);
+//36.5
+//alert(365*24*60);
+//36.6
+//alert(1024*1024);
+//36.7
+//alert(1024*1024*1024);
+//36.8
+//alert(10*1024*1024*1024);
+//36.9
+//alert(1024*1024*1024*1024);
+//36.10
+//alert(1024*1024*1024);
+
+//37.1
+// let r = 3;
+// let s = 3.14 * r**2;
+// alert(s);
+//37.2
+// let a =5;
+// let s = a**2;
+// alert(s);
+//37.3
+// let a =2;
+// let b = 3;
+// let s= a*b;
+// alert(s);
+//37.4
+// let a = 5;
+// let b = 7;
+// let p = 2 * (a + b);
+// alert(p);
+//37.5
+// let tc=100;
+// let tf = (tc * 9 / 5) + 32;
+// alert(tf);
+//37.6
+let tf = 100;
+let tc = (tf - 32) * 5 / 9;
+alert(tc);
